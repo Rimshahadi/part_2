567665d8f944000778a0505dabec0cb0a90175da
Rimsha Hadi <rimshahadi78@gmail.com>
2024-05-14 12:17:47 +0500

    Your commit message

--- python-scrapy-playbook-part-1/chocolatescraper/chocolatescraper/setup.py
+++ python-scrapy-playbook-part-1/chocolatescraper/chocolatescraper/setup.py
@@ -1,10 +1,10 @@
-# from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
-# setup(
-#     name='chocolatescraper',
-#     version='1.0',
-#     packages=find_packages(),
-#     install_requires=[
-#         'scrapy',
-#     ],
-# )
+setup(
+    name='chocolatescraper',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'scrapy',
+    ],
+)
